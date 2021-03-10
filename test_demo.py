6af1f23e22e7cb3d4aafc66999f96398c1a93f08
karen-liu01 <69795603+karen-liu01@users.noreply.github.com>
2021-03-10 12:44:37 +0800

    Update test_demo.py

--- test_demo.py
+++ test_demo.py
@@ -3,62 +3,70 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
 class TestDemo1:
-    # get_cookies 登录后获取token的方法
-    def get_cookies(self):
+    def setup(self):
         url = "https://ceshiren.com/t/topic/6223/21"
         self.driver = webdriver.Chrome()
-        self.driver.get(url)
+        # self.driver.get(url)
+        self.driver.get("https://ceshiren.com")
         self.driver.maximize_window()
         self.driver.implicitly_wait(5)
         sleep(3)
+
+    def teardown(self):
+        self.driver.quit()
+
+    def get_cookies(self):
+        self._username = "XXX"
+        self._password = "XXX"
+
         print("打开界面点击登录按钮")
         self.driver.find_element(By.XPATH, "//*[@id='ember5']/header/div/div/div[2]/span/button[2]/span").click()
         sleep(3)
-
         print("输入用户名和密码点击登录")
-        self.driver.find_element(By.XPATH, '//*[@id="login-account-name"]').send_keys("username") # 写自己的账户名
-        self.driver.find_element(By.XPATH, '//*[@id="login-account-password"]').send_keys("password")# 写自己的密码
+        self.driver.find_element(By.XPATH, '//*[@id="login-account-name"]').send_keys(self._username)
+        self.driver.find_element(By.XPATH, '//*[@id="login-account-password"]').send_keys(self._password)
         self.driver.find_element(By.XPATH, '//*[@id="login-button"]/span').click()
         sleep(7)
-
         print("获取cookie")
         self.driver.get("https://ceshiren.com/t/topic/4496/2")
         cookies = self.driver.get_cookies()
         print(cookies)
         return cookies
-    # 处理token，存储到小型数据库shelve
+
     def handle_cookies(self):
         cookies_test = self.get_cookies()
         print(cookies_test)
         db = shelve.open("cookies")
         db["cookie"] = cookies_test
         c = db["cookie"]
+        for cookie in c:
+            if "expiry" in cookie.keys():
+                cookie.pop("expiry")
         db.close()
         return c
 
-    # test_login测试代码
-    # 先将1-3行注释，将第4行打开，去获取并存储cookie
-    # 然后将1-3打开，第4行注释，从本地获取token去登录即可，在cookie有效期内使用即可
     def test_login(self):
         db = shelve.open("cookies")
         cookies = db["cookie"]
         db.close()
-        # cookies = self.handle_cookies()
-        sleep(10)
-        self.driver = webdriver.Chrome()
-        self.driver.maximize_window()
-        self.driver.get("https://ceshiren.com")
-        self.driver.implicitly_wait(5)
-        for cookie in cookies:
-            if "expiry" in cookie.keys():
-                cookie.pop("expiry")
-            self.driver.add_cookie(cookie)
         print(cookies)
+        for cookie in cookies:
+            self.driver.add_cookie(cookie)
         sleep(3)
         self.driver.get("https://ceshiren.com/t/topic/6223/21")
         sleep(3)
         print(f"***** {self.driver.title}")
-        b = self.driver.title
-        assert "到课率查询贴" in b
-        self.driver.quit()
+        title = self.driver.title
+        key = "到课率查询贴"
+        if key in title:
+            print("cookies有效，登录成功")
+            assert "到课率查询贴" in title
+        else:
+            print("cookies无效，需要重新获取并存储")
+            cookies = self.handle_cookies()
+            self.driver.get("https://ceshiren.com/t/topic/6223/21")
+            sleep(3)
+            title = self.driver.title
+            assert "到课率查询贴" in title
